4e631f3e5a46e0b1e1481e7e6aa2ca8c19dc0c49
robertof <nececuentag5tobiosep@gmail.com>
2022-03-30 07:36:11 -0600

    adding_city_column

--- cygram/posts/models.py
+++ cygram/posts/models.py
@@ -14,9 +14,18 @@ class User(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    country = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+
+    is_admin = models.BooleanField(default=False)
+
     bio = models.TextField(blank=True)
 
     birthdate = models.DateField(blank=True, null=True)
     
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return email."""
+        return self.email
